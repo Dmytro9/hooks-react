a8d60044d6bc666fada9fae23537e59ccae8ba87
Dmytro9 <dpoliuliuk@gmail.com>
2020-02-22 22:01:03 +0200

    useState

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
 import React, { useState } from 'react';
 import './App.css';
 
-function expensiveInitialState() {
-  return 10;
-}
+// function expensiveInitialState() {
+//   return 10;
+// }
+
+const initCreds = {
+  email: '',
+  password: ''
+};
 
 const App = () => {
-  const [count, setCount] = useState(10);
+  // const [count, setCount] = useState(10);
   // const [count, setCount] = useState(() => expensiveInitialState());
 
+  const [{ count, count2 }, setCount] = useState({ count: 10, count2: 20 });
+
+  const [form, setCreds] = useState(initCreds);
+
   const handleClick = () => {
-    setCount(count + 1);
+    setCount(currentState => ({
+      ...currentState,
+      count: currentState.count + 1
+    }));
+  };
+
+  const handleChange = e => {
+    setCreds({
+      ...form,
+      [e.target.name]: e.target.value
+    });
   };
 
   return (
     <div className="App">
       <button onClick={handleClick}>+</button>
-      <div>{count}</div>
+      <div>count 1: {count}</div>
+      <div>count 2: {count2}</div>
+      <input name="email" value={form.email} onChange={handleChange} />
+      <input
+        type="password"
+        name="password"
+        value={form.password}
+        onChange={handleChange}
+      />
     </div>
   );
 };
