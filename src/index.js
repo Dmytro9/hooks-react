a53a71ea6d79833c3bc2521b877c41dd7b1eef84
Dmytro Poliuliuk <dmytro.poliuliuk@ironsrc.com>
2020-04-09 21:24:46 +0300

    useRef

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import UseStateComponent from "./useState";
 import UseEffectComponent from "./useEffect";
+import UserefComponent from "./useRef";
 
 ReactDOM.render(
   <>
     <UseStateComponent />
     <UseEffectComponent />
+    <UserefComponent />
   </>,
   document.getElementById("root")
 );
---
+++ src/useRef.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect, useRef } from "react";
+
+const UserefComponent = () => {
+  const inputRef = useRef();
+
+  const isCurrent = useRef(true);
+
+  const [count, setCount] = useState(1);
+
+  useEffect(() => {
+    return () => {
+      // called when the component is going to unmount
+      isCurrent.current = false;
+    };
+  }, []);
+
+  const handleClick = () => {
+    inputRef.current.focus();
+
+    setTimeout(() => {
+      // resolve error when seting state on unmounted comnent
+      if (isCurrent.current) {
+        setCount((state) => state + 1);
+      }
+    }, 2000);
+  };
+
+  return (
+    <div className="App">
+      {count}
+      <button onClick={handleClick}>Click</button>{" "}
+      <input ref={inputRef} placeholder="name" />
+    </div>
+  );
+};
+
+export default UserefComponent;
